d9cb6dc6be1ee77663d62eea422eed3acd0cab8d
pritish19076 <pritish19076@iiitd.ac.in>
2021-06-21 22:32:21 +0530

    Completed Sample Form

--- src/components/DemoForm.js
+++ src/components/DemoForm.js
@@ -6,32 +6,51 @@ class DemoForm extends Component {
     
         this.state = {
             username: '',
-            comment: ''
+            comment: '',
+            gender: 'male',
+            pizzaorpasta: 'Pizza',
+            Oregano: false
         }
     }
 
-    handleUserChange = (event) => {
+    handleInputChange= (event) => {
+        const target = event.target;
+        const value = target.type === 'checkbox' ? target.checked : target.value;
+        const name = target.name;
         this.setState({
-            username: event.target.value
-        })
-    }
-
-    handleCommentChange = (event) => {
-        this.setState({
-            comment: event.target.value
-        })
-    }
+          [name]: value
+        });
+      }
     
     render() {
         return (
             <form>
                 <div>
                     <label htmlFor="username">Username</label>
-                    <input type="text" id="username" value={this.state.username} onChange={this.handleUserChange} />
+                    <input type="text" id="username" name="username" value={this.state.username} onChange={this.handleInputChange} />
                 </div>
                 <div>
                     <label htmlFor="comment">Comments</label>
-                    <textarea id="comment" value={this.state.comment} onChange={this.handleCommentChange}></textarea>
+                    <textarea id="comment" name="comment" value={this.state.comment} onChange={this.handleInputChange}></textarea>
+                </div>
+                <div>
+                    <label htmlFor="gender">Gender</label>
+                    <select id="gender" name="gender" value={this.state.gender} onChange={this.handleInputChange}>
+                        <option value="male">Male</option>
+                        <option value="female">Female</option>
+                        <option value="other">Other</option>                    
+                    </select>
+                </div>
+                <div>
+                    <label>Pizza or Pasta:   
+                    <input type="radio" value="Pizza" name="pizzaorpasta" checked={this.state.pizzaorpasta==="Pizza"} onChange={this.handleInputChange}/> Pizza
+                    <input type="radio" value="Pasta" name="pizzaorpasta" checked={this.state.pizzaorpasta==="Pasta"} onChange={this.handleInputChange}/> Pasta
+                    </label> 
+                </div>
+                <div>
+                    <label>Oregano:    
+                    <input type="checkbox"  name="Oregano" checked={this.state.Oregano===true} onChange={this.handleInputChange}/> 
+                    </label>
                 </div>
                 {/* TODO -- Drop-down, radio, checkbox */}
             </form>
